ae993e30a65b82dcb9453edf70fd1eda4a230d37
“manusmd” <manuel.schmid@js-tut.de>
2021-10-15 10:43:51 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import { createEnterBirthdayPage } from "./lib/enterBirthdayPage.js";
+
+// Declare a function to create the elements and logic inside the #app element
+function createApp() {
+  // Get a reference to the <div id="app"></div> element
+  const appElement = document.querySelector("#app");
+}
+
+// Run the createApp function
+createApp();
