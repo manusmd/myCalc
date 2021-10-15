e7919d6e17de84978305e7cf5667a60ef1d0e996
“manusmd” <manuel.schmid@js-tut.de>
2021-10-15 14:55:15 +0200

    Add input and buttons

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-import { createEnterBirthdayPage } from "./lib/enterBirthdayPage.js";
+import { createButtonComponent } from "./lib/calcComponent.js";
+import { createInputComponent } from "./lib/inputComponent.js";
+import { createTitleComponent } from "./lib/titleComponent.js";
 
 // Declare a function to create the elements and logic inside the #app element
 function createApp() {
   // Get a reference to the <div id="app"></div> element
   const appElement = document.querySelector("#app");
+
+  const titleComponent = createTitleComponent();
+
+  appElement.append(titleComponent);
+
+  const inputComponent = createInputComponent();
+
+  appElement.append(inputComponent);
+
+  const calcAdd = createButtonComponent("addButton", "+");
+  const calcSub = createButtonComponent("subButton", "-");
+  inputComponent.append(calcAdd, calcSub);
 }
 
 // Run the createApp function
